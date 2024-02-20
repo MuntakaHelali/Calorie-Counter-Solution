c92e2a4b440d51faf6ba3a81ab1711b9aa932a28
MuntakaH <57158277+MuntakaHelali@users.noreply.github.com>
2024-02-19 22:25:15 -0500

    Updated Script.js
    Finalized AddEntry Function

--- script.js
+++ script.js
@@ -19,5 +19,10 @@ function isInvalidInput(str){
 function addEntry(){
    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
-   const HTMLString = ``;
+   const HTMLString = `
+   <label for="${entryDropdown.value}-${entryNumber}-name"> Entry ${entryNumber} Name</label>
+   <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
+   <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+   <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories">`;
+   targetInputContainer.innerHTML += HTMLString;
 }
